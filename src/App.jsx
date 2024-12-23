6c045a4c44cad4c380f1c8cc7ac1ab3b6c011fee
ThiwankaReiss <thiwankar2003@gmail.com>
2024-12-23 20:15:26 +0530

    test 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeachableMachine from './componenets/TeachableMachine';
+import './App.css'
+function App() {
+  return (
+    <div className="App" >
+      <TeachableMachine />
+    </div>
+  );
+}
+
+export default App;
